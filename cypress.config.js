084649debcfd43df315ae997f4d1d0ea649c8ce0
NickMur <nikitka.murashov@gmail.com>
2025-05-17 12:38:03 +0300

    test html

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:5173',
-    specPattern: 'client/cypress/e2e/*.cy.js',
-    supportFile: false,
-  },
-}); 
